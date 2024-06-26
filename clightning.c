3517fd42aa0d7b04fe8b7fcbcb84f198f26c63ba
Cody Burrows <dcodyburrows@gmail.com>
2024-06-26 19:15:52 -0400

    Judge terminal size

--- clightning.c
+++ clightning.c
@@ -2,10 +2,29 @@
 #include <ncurses.h>
 
 int main(int argc, char **argv) {
+	// Enter curses mode
 	initscr();
+
+	// Disable character buffering
+	raw();
+
+	// Disable echoing input characters that could mess up the artwork
+	noecho();
+
+	// Enquire about the size of the terminal we're dealing with
+	int x, y;
+	getmaxyx(stdscr, y, x);
+
+	attron(A_BOLD);
 	printw("LIGHTNING");
+
+	// Print window size in the center
+	mvprintw(y / 2, x / 2, "%d x %d", x, y);
 	refresh();
+
 	getch();
+
+	// Exit curses mode
 	endwin();
 
 	return 0;
