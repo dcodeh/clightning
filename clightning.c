66a634ba932557a5542c4c39dc5e9407003fbf73
Cody Burrows <dcodyburrows@gmail.com>
2024-07-05 16:14:25 -0400

    print resistance map to console

--- clightning.c
+++ clightning.c
@@ -18,55 +18,6 @@ char rndchar() {
 	return lightning_chars[r];
 }
 
-void bolt(char **canvas, int x, int y, int chars, int x0, int y0) {
-	printf("Start: %d, %d\n", x0, y0);
-	while (chars > 0) {
-		printf("%d chars remaining\n", chars);
-
-		char c = rndchar();
-		int len = (rand() % 10);
-
-		if (len > chars) {
-			len = chars;
-		}
-
-		printf("%c x %d\n", c, len);
-
-		chars = chars - len;
-		while(len--) {
-			if (x0 >= 0 && y0 >= 0 && y0 < y && x0 < x) {
-				canvas[x0][y0] = c;
-				printf(" -> %c at %d, %d\n", c, x0, y0);
-			} else {
-				printf("EEK!\n");
-				break;
-			}
-
-			switch (c) {
-				case '-':
-					x0++;
-					break;
-				case '\\':
-					x0++;
-					y0++;
-					break;
-				case '/':
-					x0--;
-					y0++;
-					break;
-				case '|':
-					y0++;
-					break;
-				default:
-					printf("what\n");
-					exit(-1);
-					break;
-			}
-		}
-	}
-
-}
-
 int main(int argc, char **argv) {
 	// Enter curses mode
 	initscr();
@@ -81,23 +32,23 @@ int main(int argc, char **argv) {
 	int x, y;
 	getmaxyx(stdscr, y, x);
 
-	// TODO DCB clean up this hacky mess
-	char **canvas = malloc(x * sizeof(char *));
-	for (int i = 0; i < x; ++i) {
-		canvas[i] = malloc(y * sizeof(char));
-		memset(canvas[i], ' ', y);
-	}
-
 	srand(time(NULL));
 
-	// generate a cool looking lightning bolt of a certain length
-	int chars = rand() % (x + y / 2);
+	// TODO DCB clean up this hacky mess
+	char **canvas = malloc(x * sizeof(char *));
+	int **resistance = malloc(x * sizeof(int *));
+	for (int i = 0; i < x; ++i) {
+		canvas[i] = malloc(y * sizeof(char));
+		resistance[i] = malloc(y * sizeof(int));
+		memset(canvas[i], ' ', y);
+		for (int j = 0; j < y; ++j) {
+			resistance[i][j] = rand() % 5;
+		}
+	}
 
 	// TODO DCB pick a random spot to start in the middle 50% of the window
 	int x0 = (x / 4) + (rand() % (x / 2));
 	int y0 = (y / 4) + (rand() % (y / 2));
-
-	bolt(canvas, x, y, chars, x0, y0);
 
 	// Output the contents of the array
 	// Go home (you're drunk)
@@ -106,7 +57,7 @@ int main(int argc, char **argv) {
 
 	for (int i = 0; i < x; ++i) {
 		for (int j = 0; j < y; ++j) {
-			mvaddch(j, i, canvas[i][j]);
+			mvaddch(j, i, resistance[i][j] + '0');
 		}
 	}
 
@@ -119,8 +70,10 @@ int main(int argc, char **argv) {
 	// Hide the memory
 	for (int i = 0; i < x; ++i) {
 		free(canvas[i]);
+		free(resistance[i]);
 	}
 	free(canvas);
+	free(resistance);
 
 	return 0;
 }
