13dc802087d2c5f22799619da472f6f655974251
Cody Burrows <dcodyburrows@gmail.com>
2024-07-05 20:21:41 -0400

    LIGHTNING LIGHTNING LIGHTNING
    - Fixed a north/south/east/west blunder (I've made it to India!)
    - Lightning takes path of least resistance
    - Sometimes I still get a * printed, and I can't figure out why
    - Print lightning in bold
    - Print out resistance map if no lightning chars are present

--- clightning.c
+++ clightning.c
@@ -27,9 +27,9 @@ static char chars[] = {
 	[SOUTH] = '|',
 	[NORTH] = '|',
 	[NORTH | EAST] = '/',
+	[NORTH | WEST] = '\\',
 	[SOUTH | WEST] = '/',
 	[SOUTH | EAST] = '\\',
-	[NORTH | WEST] = '\\',
 	[EAST] = '-',
 	[WEST] = '-',
 };
@@ -37,36 +37,59 @@ static char chars[] = {
 unsigned get_direction(int x, int y) {
 	union direction dir;
 
-	dir.bits.north = (x < 0);
-	dir.bits.south = (x > 0);
-	dir.bits.east = (y > 0);
-	dir.bits.west = (y < 0);
+	dir.bits.north = (y < 0);
+	dir.bits.south = (y > 0);
+	dir.bits.east = (x > 0);
+	dir.bits.west = (x < 0);
 
 	return dir.val;
 }
 
 void bolt(char **canvas, int **resistance, int xmax, int ymax, int x, int y, int len) {
-	if (len <= 0) {
-		return;
-	}
+	int lastx = -1;
+	int lasty = -1;
 
-	// pick the path(s) of least resistance
-	int m = 10; // TODO DCB always larger than the highest resistance value
-	int xmin, ymin;
+	while (len-- > 0) {
+		// pick the path(s) of least resistance
+		int m = 10; // TODO DCB always larger than the highest resistance value
+		int xmin, ymin;
 
-	for (int i = x - 1; i > 0 && i < xmax && i < x + 1; ++i) {
-		for (int j = y - 1; j > 0 && j < ymax && j < y + 1; ++j) {
-			int r = resistance[i][j];
-			if (r < m) {
-				m = r;
-				xmin = i;
-				ymin = j;
+		for (int i = x - 1; i > 0 && i < xmax && i < x + 1; ++i) {
+			for (int j = y - 1; j > 0 && j < ymax && j < y + 1; ++j) {
+				if (i < 0 || i >= xmax || j < 0 || j >= ymax) {
+					// skip invalid indexes (around the edges and such)
+					continue;
+				}
+
+				if (lastx > 0 && lasty > 0) {
+					if (i == lastx && j == lasty) {
+						// skip the place we came from
+						continue;
+					}
+				}
+
+				if (i == x && j == y) {
+					// skip center position
+					continue;
+				}
+
+				int r = resistance[i][j];
+				if (r < m) {
+					m = r;
+					xmin = i;
+					ymin = j;
+				}
 			}
 		}
-	}
 
-	canvas[x][y] = chars[get_direction(x - xmin, y - ymin)];
-	bolt(canvas, resistance, xmax, ymax, xmin, ymin, len - 1);
+		char c = chars[get_direction(x - xmin, y - ymin)];
+		canvas[x][y] = c;
+		lastx = x;
+		lasty = y;
+		x = xmin;
+		y = ymin;
+		printf("Placed %c at %d, %d\n", c, lastx, lasty);
+	}
 }
 
 int main(int argc, char **argv) {
@@ -107,12 +130,17 @@ int main(int argc, char **argv) {
 	// Output the contents of the array
 	// Go home (you're drunk)
 	move(0, 0);
-	attron(A_BOLD);
 
 	for (int i = 0; i < x; ++i) {
 		for (int j = 0; j < y; ++j) {
-			// mvaddch(j, i, resistance[i][j] + '0');
-			mvaddch(j, i, canvas[i][j]);
+			char c = canvas[i][j];
+			if (c != ' ') {
+				attron(A_BOLD);
+				mvaddch(j, i, c);
+			} else {
+				attroff(A_BOLD);
+				mvaddch(j, i, resistance[i][j] + '0');
+			}
 		}
 	}
 
